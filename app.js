623a22a3ba1d8c58d96001cf2a67bfac907b84e6
fabiochidda <fabio.chidda99@gmail.com>
2022-02-07 22:28:16 +0100

    Modificato il ciclo (perchè il controllo dei multipli di 3 e 5 non funzionava) ed aggiunti gli stili ai quadrati

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ for (let i=1; i<=100; i++) {
 
     console.log(i)
 
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+
+        console.log( i + " is multiple of 3 and 5" )
+        const ms_list_item = `<li class="ms_square ms_square_${i} ms_red fs-3">FizzBuzz</li>`
+        ul.innerHTML += ms_list_item;
+
+    } else if (i % 3 === 0) {
 
         console.log( i + " is multiple of 3" )
-        const ms_list_item = `<li class="ms_square ms_square_${i}">Fizz</li>`
+        const ms_list_item = `<li class="ms_square ms_square_${i} ms_green fs-3">Fizz</li>`
         ul.innerHTML += ms_list_item;
 
     } else if (i % 5 === 0) {
 
-        console.log( i + " is multiple of 5" )
-        const ms_list_item = `<li class="ms_square ms_square_${i}">Buzz</li>`
-        ul.innerHTML += ms_list_item;
-
-    } else if (i % 3 === 0 && i % 5 === 0) {
-
-        console.log("is multiple of 3 and 5")
-        const ms_list_item = `<li class="ms_square ms_square_${i}">FizzBuzz</li>`
+        console.log( i + " is multiple of 5")
+        const ms_list_item = `<li class="ms_square ms_square_${i} ms_yellow fs-3">Buzz</li>`
         ul.innerHTML += ms_list_item;
 
     } else {
 
-        const ms_list_item = `<li class="ms_square ms_square_${i}">${i}</li>`
+        const ms_list_item = `<li class="ms_square ms_square_${i} ms_light_blue fs-3">${i}</li>`
         ul.innerHTML += ms_list_item;
 
     }
