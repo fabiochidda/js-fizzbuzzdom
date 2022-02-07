10252006cd961eecc68426ac2de0d5a75024b09b
fabiochidda <fabio.chidda99@gmail.com>
2022-02-07 22:02:53 +0100

    Modificato il ciclo ed aggiunta lista con lo stile base

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-for (i=1; i<=100; i++) {
+const ul = document.querySelector("ul#ms_list")
+
+for (let i=1; i<=100; i++) {
+
     console.log(i)
 
+    if (i % 3 === 0) {
 
+        console.log( i + " is multiple of 3" )
+        const ms_list_item = `<li class="ms_square ms_square_${i}">Fizz</li>`
+        ul.innerHTML += ms_list_item;
+
+    } else if (i % 5 === 0) {
+
+        console.log( i + " is multiple of 5" )
+        const ms_list_item = `<li class="ms_square ms_square_${i}">Buzz</li>`
+        ul.innerHTML += ms_list_item;
+
+    } else if (i % 3 === 0 && i % 5 === 0) {
+
+        console.log("is multiple of 3 and 5")
+        const ms_list_item = `<li class="ms_square ms_square_${i}">FizzBuzz</li>`
+        ul.innerHTML += ms_list_item;
+
+    } else {
+
+        const ms_list_item = `<li class="ms_square ms_square_${i}">${i}</li>`
+        ul.innerHTML += ms_list_item;
+
+    }
 }
+
